3dbead1701196a2bec00c423f76f956627df40fa
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-02-13 12:36:28 +1300

    Add multiple city pages

---
+++ www/src/pages/ChristchurchCo2Sa1Page.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- The page that shows the map for the Digital Twin -->
+  <div class="full-height">
+    <Co2Sa1Viewer
+      :init-lat="christchurch.latitude"
+      :init-long="christchurch.longitude"
+      :urban-area-name="christchurch.urbanAreaName"
+      :init-height="initHeight"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Co2Sa1Viewer from "@/components/Co2Sa1Viewer.vue";
+import titleMixin from "@/mixins/title";
+
+export default Vue.extend({
+  name: "ChristchurchCo2Sa1Page",
+  title: "Christchurch Map",
+  mixins: [titleMixin],
+  components: {
+    Co2Sa1Viewer,
+  },
+
+  data() {
+    return {
+      christchurch: {
+        latitude: -43.514137213246535,
+        longitude: 172.62835098005368,
+        urbanAreaName: "Christchurch"
+      },
+      initHeight: 20000
+    }
+  },
+  async mounted() {
+    // Limit scrolling on this page
+    document.body.style.overflow = "hidden"
+  },
+
+  beforeDestroy() {
+    // Reset scrolling for other pages
+    document.body.style.overflow = ""
+  },
+});
+</script>
+
+<style>
+</style>
---
+++ www/src/pages/OamaruCo2Sa1Page.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- The page that shows the map for the Digital Twin -->
+  <div class="full-height">
+    <Co2Sa1Viewer
+      :init-lat="oamaru.latitude"
+      :init-long="oamaru.longitude"
+      :urban-area-name="oamaru.urbanAreaName"
+      :init-height="initHeight"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Co2Sa1Viewer from "@/components/Co2Sa1Viewer.vue";
+import titleMixin from "@/mixins/title";
+
+export default Vue.extend({
+  name: "OamaruCo2Sa1Page",
+  title: "Oamaru Map",
+  mixins: [titleMixin],
+  components: {
+    Co2Sa1Viewer,
+  },
+
+  data() {
+    return {
+      oamaru: {
+        latitude: -45.09652014809901,
+        longitude: 170.9714064482898,
+        urbanAreaName: "oamaru"
+      },
+      initHeight: 15000
+    }
+  },
+  async mounted() {
+    // Limit scrolling on this page
+    document.body.style.overflow = "hidden"
+  },
+
+  beforeDestroy() {
+    // Reset scrolling for other pages
+    document.body.style.overflow = ""
+  },
+});
+</script>
+
+<style>
+</style>
