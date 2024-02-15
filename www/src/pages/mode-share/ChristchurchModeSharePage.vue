19d9ae7b7ba8e918a297cbed5056674747df925c
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-02-15 13:54:18 +1300

    Build skeleton for mode-share and emissions routes

---
+++ www/src/pages/mode-share/ChristchurchModeSharePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- The page that shows the map for the Christchurch area, comparing CO2 and VKT for each SA1 area -->
+  <div class="full-height">
+    <h1>ChCh mode share</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import titleMixin from "@/mixins/title";
+
+export default Vue.extend({
+  name: "ChristchurchCo2Sa1Page",
+  title: "Christchurch Map",
+  mixins: [titleMixin],
+
+  data() {
+    return {
+      christchurch: {
+        latitude: -43.514137213246535,
+        longitude: 172.62835098005368,
+        urbanAreaName: "Christchurch"
+      },
+      initHeight: 20000
+    }
+  },
+  async mounted() {
+    // Limit scrolling on this page
+    document.body.style.overflow = "hidden"
+  },
+
+  beforeDestroy() {
+    // Reset scrolling for other pages
+    document.body.style.overflow = ""
+  },
+});
+</script>
+
+<style>
+</style>
--- www/src/routes.ts
+++ www/src/routes.ts
@@ -1,11 +1,12 @@
 import {RouteConfig} from "vue-router";
-import {AboutPage, AucklandCo2Sa1Page, ChristchurchCo2Sa1Page, OamaruCo2Sa1Page, WellingtonCo2Sa1Page} from "@/pages";
+import * as pages from "@/pages";
 
 
 enum routerLocations {
   AboutPage = "ABOUT_PAGE",
   AucklandCo2Sa1Page = "AUCKLAND_CO2_SA1_PAGE",
   ChristchurchCo2Sa1Page = "CHRISTCHURCH_CO2_SA1_PAGE",
+  ChristchurchModeSharePage = "CHRISTCHURCH_MODE_SHARE_PAGE",
   OamaruCo2Sa1Page = "OAMARU_CO2_SA1_PAGE",
   RootPage = "ROOT_PAGE",
   WellingtonCo2Sa1Page = "WELLINGTON_CO2_SA1_PAGE"
@@ -16,29 +17,34 @@ enum routerLocations {
  */
 const routes: RouteConfig[] = [
   {
+    path: "/mode-share/christchurch",
+    name: routerLocations.ChristchurchModeSharePage,
+    component: pages.modeShare.Christchurch
+  },
+  {
     path: "/auckland",
     name: routerLocations.AucklandCo2Sa1Page,
-    component: AucklandCo2Sa1Page
+    component: pages.emissions.Auckland
   },
   {
     path: "/christchurch",
     name: routerLocations.ChristchurchCo2Sa1Page,
-    component: ChristchurchCo2Sa1Page
+    component: pages.emissions.Christchurch
   },
   {
     path: "/oamaru",
     name: routerLocations.OamaruCo2Sa1Page,
-    component: OamaruCo2Sa1Page
+    component: pages.emissions.Oamaru
   },
   {
     path: "/wellington",
     name: routerLocations.WellingtonCo2Sa1Page,
-    component: WellingtonCo2Sa1Page
+    component: pages.emissions.Wellington
   },
   {
     path: "/about",
     name: routerLocations.AboutPage,
-    component: AboutPage
+    component: pages.AboutPage
   },
   {
     path: '*',
