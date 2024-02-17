41c17cbd4fd38f082a2188856500c4193365ed20
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-02-18 09:28:48 +1300

    Add mode share to geoserver using new geoserver_common.py

--- initialise_db/initialise_all_data_sources.py
+++ initialise_db/initialise_all_data_sources.py
@@ -3,9 +3,10 @@ import logging
 from dotenv import load_dotenv
 
 from config import get_db_engine
-from emissions.initialise_geoserver import initialise_geoserver
+from emissions.emissions_geoserver import initialise_geoserver_emissions
 from emissions.initialise_co2_sa1s import initialise_co2_sa1s
 from mode_share.initialise_mode_share import initialise_mode_share
+from mode_share.mode_share_geoserver import initialise_geoserver_mode_share
 from setup_logging import setup_logging
 
 log = logging.getLogger(__name__)
@@ -21,7 +22,8 @@ def main():
     initialise_mode_share(engine)
     log.info("Database initialised")
     log.info("Initialising geoserver")
-    initialise_geoserver()
+    initialise_geoserver_emissions()
+    initialise_geoserver_mode_share()
     log.info("Geoserver initialised")
 
 
---
+++ initialise_db/mode_share/mode_share_geoserver.py
@@ -0,0 +1,67 @@
+import logging
+
+from config import EnvVariable
+from geoserver_common import create_workspace_if_not_exists, create_datastore_layer, create_db_store_if_not_exists
+
+log = logging.getLogger(__name__)
+
+
+def create_sa2_view(workspace_name: str, data_store_name: str):
+    create_datastore_layer(workspace_name, data_store_name, layer_name="sa2s")
+
+
+def create_mode_share_view(workspace_name: str, data_store_name: str):
+    mode_share_layer_name = "mode_share"
+    mode_share_query = f"""
+        <metadata>
+            <entry key="JDBC_VIRTUAL_TABLE">
+                <virtualTable>
+                    <name>{mode_share_layer_name}</name>
+                    <sql>SELECT &quot;SA2_code_usual_residence_address&quot;,&#xd;
+                        &quot;SA2_usual_residence&quot;,&#xd;
+                        &quot;SA2_code_workplace_address&quot;,&#xd;
+                        &quot;SA2_workplace&quot;,&#xd;
+                        &quot;Work_at_home&quot;,&#xd;
+                        &quot;Passenger_in_a_car_truck_van_or_company_bus&quot;,&#xd;
+                        (&quot;Drive_a_private_car_truck_or_van&quot; + &quot;Drive_a_company_car_truck_or_van&quot;) as Drive,&#xd;
+                        (&quot;Public_bus&quot; + &quot;Train&quot; + &quot;Ferry&quot;)                                        as Public_transport,&#xd;
+                        (&quot;Walk_or_jog&quot; + &quot;Bicycle&quot;)                                               as Active_transport,&#xd;
+                        &quot;Other&quot;,&#xd;
+                        &quot;Total&quot;&#xd;
+                        &#xd;
+                        &#xd;
+                        FROM mode_share
+                    </sql>
+                    <escapeSql>false</escapeSql>
+                    <geometry>
+                      <name>SA2_usual_residence</name>
+                      <type>Geometry</type>
+                      <srid>-1</srid>
+                    </geometry>
+                    <geometry>
+                      <name>SA2_workplace</name>
+                      <type>Geometry</type>
+                      <srid>-1</srid>
+                    </geometry>
+                </virtualTable>
+            </entry>
+        </metadata>
+    """
+    create_datastore_layer(workspace_name, data_store_name, layer_name=mode_share_layer_name,
+                           metadata_elem=mode_share_query
+)
+
+
+def initialise_geoserver_mode_share():
+    log.info("Creating SA2 mode share database views if they do not exist")
+
+    workspace_name = "sa2_mode_share"
+    create_workspace_if_not_exists(workspace_name)
+
+    db_name = EnvVariable.POSTGRES_DB
+    data_store_name = f"{db_name} PostGIS"
+    create_db_store_if_not_exists(db_name, workspace_name, data_store_name)
+
+    create_sa2_view(workspace_name, data_store_name)
+    create_mode_share_view(workspace_name, data_store_name)
+    log.info("SA2 mode share database views initialised")
