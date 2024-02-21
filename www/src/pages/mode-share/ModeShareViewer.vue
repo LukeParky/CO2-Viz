3693ce499f1f1031ba785a4fb7119dfb6952efd1
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-02-22 09:52:56 +1300

    Use database sheet id to initialise flowmap

--- www/src/pages/mode-share/ModeShareViewer.vue
+++ www/src/pages/mode-share/ModeShareViewer.vue
@@ -2,10 +2,10 @@
   <!-- The component that renders a CO2/VKT map for a given area of SA1s -->
   <div class="full-screen">
     <iframe
-      src="https://www.flowmap.blue/1oZqsz8f0VlcmeLv35_OOe9TBDrTBWx4IhGIl9lodb2Q/c2bb2e1"
+      :title="`Mode share flow map ${this.urbanAreaName}`"
+      :src=flowMapSrcUrl
       width="100%"
       height="1080px"
-      frameborder="0"
       allowfullscreen
     />
   </div>
@@ -13,9 +13,7 @@
 
 <script lang="ts">
 import axios from "axios";
-import * as Cesium from "cesium";
 import {MapViewer} from 'geo-visualisation-components/src/components';
-import {MapViewerDataSourceOptions} from "geo-visualisation-components/src/types";
 import Vue from "vue";
 
 interface Sa1Emissions {
@@ -61,39 +59,41 @@ export default Vue.extend({
 
   data() {
     return {
-      baseLayer: new Cesium.ImageryLayer(new Cesium.OpenStreetMapImageryProvider({}), {}),
       geoserverHost: `${process.env.VUE_APP_GEOSERVER_HOST}:${process.env.VUE_APP_GEOSERVER_PORT}`,
-      dataSources: {geoJsonDataSources: []} as MapViewerDataSourceOptions,
-      cesiumApiToken: process.env.VUE_APP_CESIUM_ACCESS_TOKEN,
+      sheetId: undefined as string | undefined
     }
   },
 
-  async mounted() {
-    const geojson = await this.loadSa2s()
-    this.dataSources.geoJsonDataSources = [geojson]
+  async created() {
+    this.sheetId = await this.fetchSheetId()
   },
 
   methods: {
-    async loadSa2s(): Promise<Cesium.GeoJsonDataSource> {
-      const geoserverUrl = axios.getUri({
+    async fetchSheetId(): Promise<string> {
+      const propertyRequestUrl = axios.getUri({
         url: `${this.geoserverHost}/geoserver/sa2_mode_share/ows`,
         params: {
           service: "WFS",
           version: "1.0.0",
           request: "GetFeature",
           outputFormat: "application/json",
-          typeName: "sa2_mode_share:sa2s",
-          cql_filter: `UR2023_V1_00_NAME ILIKE '${this.urbanAreaName}'`
+          typeName: "sa2_mode_share:flow_sheets",
+          propertyname: "(sheet_url)",
+          cql_filter: `urban_area ILIKE '${this.urbanAreaName}'`
         }
       })
-
-      return Cesium.GeoJsonDataSource.load(geoserverUrl, {
-        fill: Cesium.Color.fromAlpha(Cesium.Color.ROYALBLUE, 1),
-        stroke: Cesium.Color.ROYALBLUE.darken(0.5, new Cesium.Color()),
-        strokeWidth: 10
-      });
-    },
+      const propertyJson = await axios.get(propertyRequestUrl)
+      const features = propertyJson.data.features as { properties: { urban_area: string, sheet_url: string } }[]
+      const sheet_url = new URL(features[0].properties.sheet_url)
+      return sheet_url.pathname.split("/").pop() as string
+    }
   },
+
+  computed: {
+    flowMapSrcUrl(): string {
+      return `https://www.flowmap.blue/${this.sheetId}`
+    }
+  }
 
 });
 </script>
