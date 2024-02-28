3ee71e9daa88d2a0757d6f35468f8c50e80637f2
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-02-29 08:13:31 +1300

    Added rounded spinner that works for each row

---
+++ www/src/components/RoundedSpinner.vue
@@ -0,0 +1,45 @@
+<template>
+  <input
+    ref="spinner"
+    type="number"
+    :value="spinnerDisplayValue"
+    :min="min"
+    :max="max"
+    @focusout="$emit('input', $event)"
+    @keyup.enter="$emit('input', $event)"
+  >
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {roundToFixed} from "@/utils";
+
+export default Vue.extend({
+  name: "RoundedSpinner",
+
+  props: {
+    value: Number,
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 100,
+    },
+    decimalPlaces: {
+      type: Number,
+      default: 2,
+    },
+  },
+
+  computed: {
+    spinnerDisplayValue(): string {
+      return roundToFixed(this.value, this.decimalPlaces)
+    }
+  }
+});
+</script>
+
+<style scoped>
+</style>
