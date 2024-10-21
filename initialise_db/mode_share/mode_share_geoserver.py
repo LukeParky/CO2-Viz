aae287bda02cf0d66e66a4d8d0e89bbc614f09b9
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-10-22 08:58:42 +1300

    Use escaped sql query

--- initialise_db/mode_share/mode_share_geoserver.py
+++ initialise_db/mode_share/mode_share_geoserver.py
@@ -12,23 +12,31 @@ def create_sa2_view(workspace_name: str, data_store_name: str):
 
 def create_mode_share_view(workspace_name: str, data_store_name: str):
     mode_share_layer_name = "mode_share"
+
+    # Construct query linking medusa_table_class to its geometry table
+    mode_share_sql_query = f"""
+        SELECT
+            "SA2_code_usual_residence_address",
+            "SA2_code_workplace_address",
+            "Work_at_home",
+            "Passenger_in_a_car_truck_van_or_company_bus",
+            ("Drive_a_private_car_truck_or_van", "Drive_a_company_car_truck_or_van") AS "Drive",
+            ("Public_bus" + "Train" + "Ferry") AS "Public_transport",
+            ("Walk_or_jog" + "Bicycle") AS "Active_transport",
+            "Other",
+            "Total"
+        FROM {mode_share_layer_name}
+    """
+    # Escape characters in SQL query so that it is valid Geoserver XML
+    xml_escaped_sql = saxutils.escape(mode_share_sql_query, entities={r"'": "&apos;", "\n": "&#xd;"})
+
     mode_share_query = f"""
         <metadata>
             <entry key="JDBC_VIRTUAL_TABLE">
                 <virtualTable>
                     <name>{mode_share_layer_name}</name>
-                    <sql>SELECT &quot;SA2_code_usual_residence_address&quot;,&#xd;
-                        &quot;SA2_code_workplace_address&quot;,&#xd;
-                        &quot;Work_at_home&quot;,&#xd;
-                        &quot;Passenger_in_a_car_truck_van_or_company_bus&quot;,&#xd;
-                        (&quot;Drive_a_private_car_truck_or_van&quot; + &quot;Drive_a_company_car_truck_or_van&quot;) as Drive,&#xd;
-                        (&quot;Public_bus&quot; + &quot;Train&quot; + &quot;Ferry&quot;)                                        as Public_transport,&#xd;
-                        (&quot;Walk_or_jog&quot; + &quot;Bicycle&quot;)                                               as Active_transport,&#xd;
-                        &quot;Other&quot;,&#xd;
-                        &quot;Total&quot;&#xd;
-                        &#xd;
-                        &#xd;
-                        FROM mode_share
+                    <sql>
+                        {xml_escaped_sql}
                     </sql>
                     <escapeSql>false</escapeSql>
                 </virtualTable>
